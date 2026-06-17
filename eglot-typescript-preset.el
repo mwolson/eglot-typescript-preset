cc780650223cf08f2268bca3de16509c61ef885d
Mike Olson <mwolson@member.fsf.org>
2026-06-16 21:28:31 -0400

    chore: bump version to 1.0.0

--- eglot-typescript-preset.el
+++ eglot-typescript-preset.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) 2026 Michael Olson <mwolson@gnu.org>
 
-;; Version: 0.6.0
+;; Version: 1.0.0
 ;; Author: Michael Olson <mwolson@gnu.org>
 ;; Maintainer: Michael Olson <mwolson@gnu.org>
 ;; URL: https://github.com/mwolson/eglot-typescript-preset
